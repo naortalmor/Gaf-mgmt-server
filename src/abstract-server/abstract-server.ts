0dd4bfac11fafb1206325ad5cec4d7193d673be1
Lior <42876247+lior21@users.noreply.github.com>
2020-03-11 13:55:47 +0200

    fix abstract server

--- src/abstract-server/abstract-server.ts
+++ src/abstract-server/abstract-server.ts
@@ -6,34 +6,30 @@ import * as admin from 'firebase-admin';
 export class AbstractServer {
     static db;
 
-    static init(callback:Function) {
+    static init(callback: Function) {
         let app = express();
         app.use(bodyParser.json());
         AbstractServer.initCors(app);
         env.config();
         const port = process.env.PORT || 1112;
         this.initDbConnection();
-        app.listen(port, (err) => {
-            if(err) {
-                console.log(err)
-            } else {
-                console.log(`server is listening on port: ${port}`);
-                callback(app);
-            }
+        app.listen(port, () => {
+            console.log(`server is listening on port: ${port}`);
+            callback(app);
         });
     }
 
-    private static initDbConnection():void {
+    private static initDbConnection(): void {
         admin.initializeApp({
             credential: admin.credential.applicationDefault(),
             databaseURL: 'https://gaf-mgmt.firebaseio.com/'
-          });
+        });
 
         AbstractServer.db = admin.database();
     }
 
-    private static initCors(app:express.Application) {
-        app.use((req,res,next) => {
+    private static initCors(app: express.Application) {
+        app.use((req, res, next) => {
             res.setHeader('Content-Type', 'text/plain');
             res.header("Access-Control-Allow-Origin", "*");
             res.header("Access-Control-Allow-Headers", "*");
