cc5e60d7f066dd397c6406aee371f6eec3b7cf9f
Saeed <saeedam>
2020-03-12 04:39:55 +0200

    aa

--- src/modules/launches/api.ts
+++ src/modules/launches/api.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import {Request, Response} from "express";
+import { Request, Response } from 'express';
 import { AbstractServer } from '../../abstract-server/abstract-server';
 
 
@@ -9,11 +9,11 @@ export class LaunchesApi {
             try {
                 let restaurantRef = AbstractServer.db.ref('restaurant');
                 restaurantRef.on('value', (restaurants) => {
-                    res.status(200).send(Object.values(restaurants.val()).filter(curr => curr))
-                })
+                    res.status(200).send(Object.values(restaurants.val()).filter(curr => curr));
+                });
             } catch (error) {
                 console.log(`Error with getting all restaurants - ${error}`);
-                res.status(500).send(`Error with getting all restaurants - ${error}`)
+                res.status(500).send(`Error with getting all restaurants - ${error}`);
             }
         });
 
@@ -25,8 +25,32 @@ export class LaunchesApi {
                 res.send(newRestaurant);
             } catch (error) {
                 console.log(`Error with saving new restaurant - ${error}`);
-                res.status(500).send(`Error with saving new restaurant - ${error}`)
+                res.status(500).send(`Error with saving new restaurant - ${error}`);
             }
-        })
+        });
+
+        app.post('/launches/updateRestaurantSurveyStatus', (req:Request, res:Response) => {
+            try {
+                let statusesRef = AbstractServer.db.ref('statuses');
+                const newStatus = req.body.newStatus;
+
+                statusesRef.update({restaurantSurveyStatus:newStatus}).then(result => res.send(newStatus)).catch(error => res.status(500).send(`Error with saving new restaurant - ${error}`));
+            } catch (error) {
+                console.log(`Error with saving new restaurant - ${error}`);
+                res.status(500).send(`Error with saving new restaurant - ${error}`);
+            }
+        });
+
+        app.get('/launches/getRestaurantSurveyStatus', (req:Request, res:Response) => {
+            try {
+                let statusesRef = AbstractServer.db.ref('statuses');
+                statusesRef.on('value', (status) => {
+                    res.status(200).send(Object.values(status.val()).filter(curr => curr));
+                });
+            } catch (error) {
+                console.log(`Error with getting all restaurants - ${error}`);
+                res.status(500).send(`Error with getting all restaurants - ${error}`);
+            }
+        });
     }
 }
