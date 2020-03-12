1187c0642093bd3478e6f27162c87cb924f57657
Naor Talmor <naortalmornew@gmail.com>
2020-03-12 06:03:42 +0200

    event db

--- src/modules/evenings/api.ts
+++ src/modules/evenings/api.ts
@@ -11,9 +11,35 @@ export class EveningsApi {
                     res.status(200).send(Object.values(evenings.val()).filter(curr => curr))
                 })
             } catch (error) {
-                console.log(`Error with getting all restaurants - ${error}`);
-                res.status(500).send(`Error with getting all restaurants - ${error}`)
+                console.log(`Error with getting all evenings - ${error}`);
+                res.status(500).send(`Error with getting all evenings - ${error}`)
             }
         });
+
+        app.get('/events/getAllEvents', (req:Request, res:Response) => {
+            try {
+                let eventsRef = AbstractServer.db.ref('calendar-event');
+                eventsRef.on('value', (events) => {
+                    res.status(200).send(Object.values(events.val()).filter(curr => curr))
+                })
+            } catch (error) {
+                console.log(`Error with getting all events - ${error}`);
+                res.status(500).send(`Error with getting all events - ${error}`)
+            }
+        })
+
+        app.post('/evenings/addEvent', (req:Request, res:Response) => {
+            try {
+                let eventsRef = AbstractServer.db.ref('calendar-event').push();
+                const newCalenderEvent = {...req.body, id: eventsRef.key};
+                eventsRef.set(newCalenderEvent).then(() => {
+                    res.send(newCalenderEvent)
+                }
+                );
+            } catch (error) {
+                console.log(`Error with saving new restaurant - ${error}`);
+                res.status(500).send(`Error with saving new restaurant - ${error}`)
+            }
+        })
     }
 }
