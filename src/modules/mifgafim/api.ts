30cbf8f595ab6f3b1144c29dc59ac31b561bedb7
Saeed <saeedam>
2020-03-20 15:45:44 +0200

    aa

--- src/modules/mifgafim/api.ts
+++ src/modules/mifgafim/api.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import {Request, Response} from "express";
+import { Request, Response } from 'express';
 import { getHistory } from './src/history';
 import { thisWeekPersons } from './src/this-week-persons';
 import { generateNextMifgafs } from './src/justice';
@@ -13,19 +13,13 @@ export class MifgafimApi {
         app.get('/mifgafim/thisWeekPersons', (req:Request, res:Response) => {
             res.send(thisWeekPersons());
         });
-        app.get('/mifgafim/getAllUsers', (req:Request, res:Response) => {
-            let usersRef = AbstractServer.db.ref('users');
-            usersRef.on('value', (users) => {
-                res.status(200).send(users.val())
-            })
-        });
         app.get('/mifgafim/winners', (req:Request, res:Response) => {
             let usersRef = AbstractServer.db.ref('winners');
             usersRef.on('value', (winners) => {
                 res.send((winners.val()));
-            })
-            
-        })
+            });
+
+        });
         app.get('/mifgafim/generateNextMifgafs/:howMuch', async (req:Request, res:Response) => {
             let howMuch:number = parseInt(req.params.howMuch);
             if (!isNaN(howMuch)) {
